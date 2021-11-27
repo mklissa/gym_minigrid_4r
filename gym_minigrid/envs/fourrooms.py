1318ebb16f26564d81822c079202438e111ea3ab
Martin Klissarov <martinklissarov@Martins-MacBook-Air.local>
2021-11-27 10:40:39 -0500

    smalelr 4r

--- gym_minigrid/envs/fourrooms.py
+++ gym_minigrid/envs/fourrooms.py
@@ -11,10 +11,10 @@ class FourRoomsEnv(MiniGridEnv):
     Can specify agent and goal position, if not it set at random.
     """
 
-    def __init__(self, agent_pos=np.array([3,4]), goal_pos=np.array([15,15])):
+    def __init__(self, agent_pos=np.array([3,3]), goal_pos=np.array([9,9])):
         self._agent_default_pos = agent_pos
         self._goal_default_pos = goal_pos
-        super().__init__(grid_size=19, max_steps=1000)
+        super().__init__(grid_size=13, max_steps=100)
 
     def _gen_grid(self, width, height):
         # Create the grid
@@ -42,13 +42,13 @@ class FourRoomsEnv(MiniGridEnv):
                 # Bottom wall and door
                 if i + 1 < 2:
                     self.grid.vert_wall(xR, yT, room_h)
-                    pos = (xR, yT +4) #self._rand_int(yT + 1, yB))
+                    pos = (xR, yT +3) #self._rand_int(yT + 1, yB))
                     self.grid.set(*pos, None)
 
                 # Bottom wall and door
                 if j + 1 < 2:
                     self.grid.horz_wall(xL, yB, room_w)
-                    pos = (xL+5, yB) #(self._rand_int(xL + 1, xR), yB)
+                    pos = (xL+3, yB) #(self._rand_int(xL + 1, xR), yB)
                     self.grid.set(*pos, None)
 
         # Randomize the player start position and orientation
